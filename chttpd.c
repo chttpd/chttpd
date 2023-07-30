e1a70f22933398d31cf95bcec74fb56990974bbb
Hossein Sokhandan <iamsokhandan@gmail.com>
2023-07-30 16:55:49 +0330

    Implement carrow coroutines as chttpd handlers

--- chttpd.c
+++ chttpd.c
@@ -28,6 +28,8 @@
 typedef struct carrow_microhttpd {
     struct MHD_Daemon *daemon;
     struct MHD_Connection *connection;
+    struct MHD_Response *response;
+    int processed;
 } carrow_microhttpd;
 
 
@@ -45,47 +47,53 @@ struct Route {
 };
 
 
-void
-handle_hello(struct carrow_microhttpd_coro *self,
-        struct carrow_microhttpd *state) {
-    const char *msg = "Hello, world!";
-    struct MHD_Response *response = MHD_create_response_from_buffer(
-        strlen(msg), (void *) msg, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(state->connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
+static int
+maketimer(unsigned int interval) {
+    int fd = timerfd_create(CLOCK_REALTIME, TFD_NONBLOCK);
+    if (fd == -1) {
+        return -1;
+    }
+
+    struct timespec sec1 = {interval, 0};
+    struct itimerspec spec = {sec1, sec1};
+    if (timerfd_settime(fd, 0, &spec, NULL) == -1) {
+        return -1;
+    }
+    return fd;
 }
 
 
 void
 handle_goodbye(struct carrow_microhttpd_coro *self,
         struct carrow_microhttpd *state) {
-    const char *msg = "Goodbye, world!";
-    struct MHD_Response *response = MHD_create_response_from_buffer(
-        strlen(msg), (void *) msg, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(state->connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
-}
+    CORO_START;
+    unsigned long tmp;
+    ssize_t bytes;
 
-
-void
-handle_echo(struct carrow_microhttpd_coro *self,
-        struct carrow_microhttpd *state) {
-    const char *data = MHD_lookup_connection_value(state->connection,
-            MHD_GET_ARGUMENT_KIND, "data");
-    if (data == NULL) {
-        data = "No data provided";
+    self->fd = maketimer(1);
+    if (self->fd == -1) {
+        CORO_REJECT("maketimer");
     }
-    struct MHD_Response *response = MHD_create_response_from_buffer(
-            strlen(data), (void *) data, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(state->connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
+
+    CORO_WAIT(self->fd, CIN);
+    bytes = read(self->fd, &tmp, sizeof(tmp));
+    DEBUG("Read bytes %zd.", bytes);
+    const char *msg = "Goodbye, world!";
+    state->response = MHD_create_response_from_buffer(
+            strlen(msg), (void *) msg, MHD_RESPMEM_PERSISTENT);
+    state->processed = 1;
+
+    CORO_FINALLY;
+    if (self->fd != -1) {
+        carrow_microhttpd_evloop_unregister(self->fd);
+        close(self->fd);
+    }
+    CORO_END;
 }
 
 
 struct Route routing_table[] = {
-    {"/hello", "GET", handle_hello},
     {"/goodbye", "GET", handle_goodbye},
-    {"/echo", "GET", handle_echo},
     {NULL, NULL, NULL}
 };
 
@@ -104,16 +112,31 @@ request_handler(void *cls, struct MHD_Connection *connection, const char *url,
         if (strcmp(url, routing_table[i].url) == 0 &&
                 strcmp(method, routing_table[i].method) == 0) {
             struct carrow_microhttpd handler_state = {
-                .connection = connection
+                .connection = connection,
+                .response = NULL,
+                .processed = 0
             };
             carrow_microhttpd_coro_create_and_run(routing_table[i].handler,
                     &handler_state);
+
+            if (handler_state.processed == 1) {
+                if (handler_state.response != NULL) {
+                    ret = MHD_queue_response(handler_state.connection,
+                            MHD_HTTP_OK, handler_state.response);
+                    MHD_destroy_response(handler_state.response);
+                    return ret;
+                } else {
+                    // Error happened.
+                }
+            } else {
+                // Tell MHD to wait
+            }
         }
     }
 
     const char *not_found = "Not found";
     response = MHD_create_response_from_buffer(
-        strlen(not_found), (void *) not_found, MHD_RESPMEM_PERSISTENT);
+            strlen(not_found), (void *) not_found, MHD_RESPMEM_PERSISTENT);
     ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
     MHD_destroy_response(response);
     return ret;
