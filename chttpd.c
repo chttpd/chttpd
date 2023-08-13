5cc0289b9c875d5ef55987cfe2f3875e14f37214
Hossein Sokhandan <42068956+hosseinsokhandan@users.noreply.github.com>
2023-08-13 12:25:05 +0330

    Feature: CLI -b/--bind and -p/--port (#4)
    * Add cli support
    
    * Add --help for cli
    
    * Remove redundant semicolon
    
    * Remove extra space before parentheses
    
    * Use argparse instead of getopt
    
    * Fix cli help texts for -b and -p

--- chttpd.c
+++ chttpd.c
@@ -18,11 +18,33 @@
  */
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/timerfd.h>
+#include <argp.h>
+#include <arpa/inet.h>
 
 #include <microhttpd.h>
 #include <clog.h>
 #include <carrow.h>
+
+#include "manifest.h"
+
+
+const char *argp_program_version = CHTTPD_VERSION;
+const char *argp_program_bug_address = "<vahid.mardani@gmail.com>";
+static char doc[] = "Nonblocking IO HTTP server using Carrow.";
+static char args_doc[] = "";
+
+
+static struct argp_option options[] = {
+    {"port",  'p', "PORT",         0,  "Specify chttpd listening port"},
+    {"bind",  'b', "BIND ADDRESS", 0,  "Specify chttpd binding address"},
+    {0}
+};
+
+
+struct arguments {
+    char bind_addr[32];
+    int port;
+};
 
 
 typedef struct carrow_microhttpd {
@@ -54,6 +76,52 @@ request_handler(void *cls, struct MHD_Connection *connection, const char *url,
 
     MHD_destroy_response(response);
     return ret;
+}
+
+
+void
+parse_port(const char *port_arg, int *port) {
+    bool is_valid = true;
+
+    for (int i = 0; i < strlen(port_arg); i++) {
+        if (port_arg[i] < '0' || port_arg[i] > '9') {
+            is_valid = false;
+            break;
+        }
+    }
+
+    if (is_valid == false) {
+        ERROR("Invalid port number: %s\n", port_arg);
+        exit(EXIT_FAILURE);
+    }
+
+    *port = atoi(port_arg);
+}
+
+
+void
+parse_bind_addr(const char *bind_addr_arg, char *bind_addr, int *port) {
+    int colon_pos = -1;
+
+    if (strcmp(bind_addr_arg, "localhost") == 0) {
+        bind_addr_arg = "127.0.0.1";
+    }
+
+    for (int i = 0; i < strlen(bind_addr_arg); i++) {
+        if (bind_addr_arg[i] == ':') {
+            colon_pos = i;
+            break;
+        }
+    }
+
+    if (colon_pos == -1) {
+        strcpy(bind_addr, bind_addr_arg);
+    } else {
+        strncpy(bind_addr, bind_addr_arg, colon_pos);
+        bind_addr[colon_pos] = '\0';
+
+        parse_port(bind_addr_arg + colon_pos + 1, port);
+    }
 }
 
 
@@ -98,23 +166,56 @@ httpserverA(struct carrow_microhttpd_coro *self,
 }
 
 
-int
-main() {
-    struct MHD_Daemon *d;
-    int port;
+static error_t
+parse_opt(int key, char *arg, struct argp_state *state) {
+  struct arguments *arguments = state->input;
 
-    port = 8080;
+  switch (key) {
+    case 'p':
+      parse_port(arg, &arguments->port);
+      break;
+    case 'b':
+      parse_bind_addr(arg, arguments->bind_addr, &arguments->port);
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+
+int
+main(int argc, char *argv[]) {
+    struct MHD_Daemon *d;
+    struct sockaddr_in daemon_addr;
+    struct arguments arguments;
+
+    /* Default values. */
+    arguments.port = 8080;
+    strcpy(arguments.bind_addr, "127.0.0.1");
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    memset(&daemon_addr, 0, sizeof(struct sockaddr_in));
+
     clog_verbosity = CLOG_DEBUG;
 
-    d = MHD_start_daemon(MHD_USE_DEBUG, port, NULL, NULL, &request_handler,
-            NULL, MHD_OPTION_END);
+    daemon_addr.sin_family = AF_INET;
+    daemon_addr.sin_port = htons(arguments.port);
+    daemon_addr.sin_addr.s_addr = inet_addr(arguments.bind_addr);
+
+    d = MHD_start_daemon(MHD_USE_ERROR_LOG, 0, NULL, NULL, &request_handler,
+            NULL, MHD_OPTION_SOCK_ADDR, &daemon_addr, MHD_OPTION_END);
 
     if (d == NULL) {
-        ERROR("Could not start daemon on port %d.\n", port);
+        ERROR("Could not start daemon on port %d.\n", arguments.port);
         return 1;
     }
 
-    INFO("Listening on port %d...\n", port);
+    INFO("Listening on port %d...\n", arguments.port);
 
     struct carrow_microhttpd state = {
         .daemon = d
