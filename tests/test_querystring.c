3c8c5949056637f25f491a037dcc6e53418885a0
pylover <vahid.mardani@gmail.com>
2023-08-14 14:49:04 +0330

    Query string basic tests

--- tests/test_querystring.c
+++ tests/test_querystring.c
@@ -21,17 +21,61 @@
 #include <cutest.h>
 
 
+int
+qstok(char *query, char **saveptr, char **key, char **value);
+
+
 void
 test_querystring() {
-    eqint(2, 2);
-    eqint(2, 2);
-    eqint(2, 2);
-    eqint(2, 2);
-    eqint(2, 1);
+    char *saveptr;
+    const char *query = "foo=bar&baz=quux";
+    char *copy = malloc(strlen(query) + 1);
+    strcpy(copy, query);
+
+    char *key = NULL;
+    char *value = NULL;
+
+    eqint(0, qstok(copy, &saveptr, &key, &value));
+    eqint(3, strlen(key));
+    eqint(3, strlen(value));
+    eqstr("foo", key);
+    eqstr("bar", value);
+
+    eqint(0, qstok(NULL, &saveptr, &key, &value));
+    eqint(3, strlen(key));
+    eqint(4, strlen(value));
+    eqstr("baz", key);
+    eqstr("quux", value);
+
+    eqint(1, qstok(NULL, &saveptr, &key, &value));
+    free(copy);
+}
+
+
+void
+test_querystring_error() {
+    const char *query = "foo=bar&baz=quux";
+    char *copy = malloc(strlen(query) + 1);
+    strcpy(copy, query);
+    char *saveptr = copy;
+
+    char *key;
+    char *value;
+
+    eqint(-1, qstok(copy, &saveptr, &key, &value));
+    saveptr = NULL;
+    eqint(-1, qstok(NULL, &saveptr, &key, &value));
+    eqint(-1, qstok(copy, &saveptr, NULL, &value));
+    eqint(-1, qstok(copy, &saveptr, &key, NULL));
 }
 
 
 void
 main() {
     test_querystring();
+    test_querystring_error();
+
+    // TODO:
+    // test_querystring_encodded();
+    return EXIT_SUCCESS;
 }
