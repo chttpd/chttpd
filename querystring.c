e44dd8504aafb04e9a53af2792fde2c2782aaeb2
Hossein <42068956+hosseinsokhandan@users.noreply.github.com>
2023-08-19 11:44:28 +0330

    Merge feature/querystring with master (#10)
    * Query string basic tests
    
    * Feature/querystring (#7)
    
    * Implement tests for querystring.c
    
    * Remove extra spaces
    
    * Remove cutest.h
    
    * Fix not allocating memory on decode
    
    * free copy in test_querystring_error
    
    * return -1 instead of exit()
    
    * Rename decode to qsdecode
    
    * Move qstok and qsdecode to querystring.c
    
    ---------
    
    Co-authored-by: pylover <vahid.mardani@gmail.com>

--- querystring.c
+++ querystring.c
@@ -17,10 +17,16 @@
  *  Author: Vahid Mardani <vahid.mardani@gmail.com>
  */
 #include <stdlib.h>
+#include <string.h>
 
-#include <cutest.h>
 
-
+/**
+ * @brief This function decodes a URL-encoded string.
+ *        It takes a pointer to the URL-encoded string, and decodes it in an
+ *        in-place manner.
+ *
+ * @param encoded The URL-encoded string
+ */
 int
 qsdecode(char *encoded) {
     size_t length = strlen(encoded);
@@ -53,6 +59,17 @@ qsdecode(char *encoded) {
 }
 
 
+/**
+ * @brief This function is responsible for parsing querystring.
+ *        Returns -1 on error, returns 0 on success which means you must stil
+ *        call to get the subsequent key/value back, and returns 1 indicating
+ *        end of querystring.
+ *
+ * @param query Pointer to the query in memory.
+ * @param saveptr See strtok_r().
+ * @param key Representing the key of querystring.
+ * @param value Representing the value of querystring.
+ */
 int
 qstok(char *query, char **saveptr, char **key, char **value) {
     if ((query != NULL) && (*saveptr != NULL)) {
@@ -82,89 +99,4 @@ qstok(char *query, char **saveptr, char **key, char **value) {
     }
 
     return 0;
-}
-
-
-void
-test_querystring() {
-    const char *query = "foo=bar&baz=quux";
-
-    char *copy = malloc(strlen(query) + 1);
-    strcpy(copy, query);
-
-    char *saveptr;
-    char *key = NULL;
-    char *value = NULL;
-
-    eqint(0, qstok(copy, &saveptr, &key, &value));
-    eqint(3, strlen(key));
-    eqint(3, strlen(value));
-    eqstr("foo", key);
-    eqstr("bar", value);
-
-    eqint(0, qstok(NULL, &saveptr, &key, &value));
-    eqint(3, strlen(key));
-    eqint(4, strlen(value));
-    eqstr("baz", key);
-    eqstr("quux", value);
-
-    eqint(1, qstok(NULL, &saveptr, &key, &value));
-    free(copy);
-}
-
-
-void
-test_querystring_error() {
-    const char *query = "foo=bar&baz=quux";
-
-    char *copy = malloc(strlen(query) + 1);
-    strcpy(copy, query);
-
-    char *saveptr = copy;
-    char *key;
-    char *value;
-
-    eqint(-1, qstok(copy, &saveptr, &key, &value));
-    saveptr = NULL;
-    eqint(-1, qstok(NULL, &saveptr, &key, &value));
-    eqint(-1, qstok(copy, &saveptr, NULL, &value));
-    eqint(-1, qstok(copy, &saveptr, &key, NULL));
-
-    free(copy);
-}
-
-
-void
-test_querystring_url_encoded() {
-    const char *query = "foo%20bar=baz&qux=quux%21corge";
-
-    char *copy = malloc(strlen(query));
-    strcpy(copy, query);
-
-    char *saveptr;
-    char *key;
-    char *value;
-
-    eqint(0, qstok(copy, &saveptr, &key, &value));
-    eqint(7, strlen(key));
-    eqint(3, strlen(value));
-    eqstr("foo bar", key);
-    eqstr("baz", value);
-
-    eqint(0, qstok(NULL, &saveptr, &key, &value));
-    eqint(3, strlen(key));
-    eqint(10, strlen(value));
-    eqstr("qux", key);
-    eqstr("quux!corge", value);
-
-    eqint(1, qstok(NULL, &saveptr, &key, &value));
-    free(copy);
-}
-
-
-void
-main() {
-    test_querystring();
-    test_querystring_error();
-    test_querystring_url_encoded();
 }
