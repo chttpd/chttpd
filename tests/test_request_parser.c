0799dd13cd518360fcd61ee5de3d7df85f4bc53c
Hossein Sokhandan <iamsokhandan@gmail.com>
2023-08-20 01:35:06 +0330

    Implement reqtok and headtok tests

--- tests/test_request_parser.c
+++ tests/test_request_parser.c
@@ -17,42 +17,163 @@
  *  Author: Vahid Mardani <vahid.mardani@gmail.com>
  */
 #include <stdlib.h>
+
 #include <cutest.h>
+#include <clog.h>
 
 
 int
-headparse(char *headers);
+headtok(char *headers, char **saveptr, char **key, char **value) {
+    if ((headers != NULL) && (*saveptr != NULL)) {
+        /* **saveptr must be null in the first try */
+        return -1;
+    }
 
+    if ((*saveptr == NULL) && (headers == NULL)) {
+        /* headers is not provided to headtok. */
+        return -1;
+    }
 
-int
-reqparse(char **verb, char **path, char **version, char **headers,
-        char **body);
+    if ((key == NULL) || (value == NULL)) {
+        /* key/value provided to headtok cannot be NULL. */
+        return -1;
+    }
 
+    *key = strtok_r(headers, ":", saveptr);
+    *value = strtok_r(NULL, "\r\n", saveptr);
 
-void
-test_request_parser() {
-    const char* request_str = "POST /api/users HTTP/1.1\r\n"
-                              "Host: example.com\r\n"
-                              "Content-Type: application/json\r\n"
-                              "\r\n"
-                              "{\"foo\":\"bar\",\"baz\":30}";
+    if ((*key == NULL) || (*value == NULL)) {
+        return 1;
+    }
+    
+    while (((**key) == ' ') || ((**key) == '\r') || ((**key) == '\n')) {
+        (*key)++;
+    }
 
-    char *verb;
-    char *path;
-    char *version;
-    char *body;
-
-    eqint(0, reqparse(&verb, &path, &version, &headers, &body));
-    eqstr(verb, "POST");
-    eqstr(path, "/api/users");
-    eqstr(version, "HTTP/1.1");
-    // eqstr(headers, ...);
-    eqstr(body, "{\"foo\":\"bar\",\"baz\":30}");
+    while ((**value) == ' ' || ((**value) == '\r') || ((**value) == '\n')) {
+        (*value)++;
+    }
+    
+    return 0;
 }
 
 
 int
+reqtok(char *request, char **saveptr, char **verb, char **path,
+        char **version) {
+    char *token;
+
+    if (*saveptr != NULL) {
+        /* **saveptr must be null. */
+        return -1;
+    }
+
+    if (request == NULL) {
+        /* request is not provided to reqtok. */
+        return -1;
+    }
+
+    /* Parse the HTTP verb. */
+    token = strtok_r(request, " ", saveptr);
+    if (token != NULL) {
+        *verb = token;
+    }
+    else {
+        return -1;
+    }
+
+    /* Parse the URL. */
+    token = strtok_r(NULL, " ", saveptr);
+    if (token != NULL) {
+        *path = token;
+    }
+    else {
+        return -1;
+    }
+
+    /* Parse request body. */
+    token = strtok_r(NULL, "/", saveptr);
+    if (token != NULL) {
+        token = strtok_r(NULL, "\r\n", saveptr);
+        if (token != NULL) {
+            *version = token;
+        }
+        else {
+            return -1;
+        }
+    }
+    else {
+        return -1;
+    }
+
+    
+    return 0;
+}
+
+
+void
+test_reqtok() {
+    const char *request = "POST /api/users HTTP/1.1\r\n";
+
+    char *copy = malloc(strlen(request) + 1);
+    strcpy(copy, request);
+
+    char *saveptr;
+    char *verb;
+    char *path;
+    char *version;
+
+    eqint(0, reqtok(copy, &saveptr, &verb, &path, &version));
+    eqstr(verb, "POST");
+    eqstr(path, "/api/users");
+    eqstr(version, "1.1");
+}
+
+
+void
+test_headtok() {
+    const char *request = "Host: foo.bar\r\nContent-Type: baz/qux\r\n"
+        "Connection: corge\r\nContent-Length: 99\r\n";
+
+    char *copy = malloc(strlen(request) + 1);
+    strcpy(copy, request);
+    
+    char *saveptr = NULL;
+    char *key;
+    char *value;
+
+    eqint(0, headtok(copy, &saveptr, &key, &value));
+    eqstr(key, "Host");
+    eqint(strlen(key), 4);
+    eqstr(value, "foo.bar");
+    eqint(strlen(value), 7);
+
+    eqint(0, headtok(NULL, &saveptr, &key, &value));
+    eqstr(key, "Content-Type");
+    eqint(strlen(key), 12);
+    eqstr(value, "baz/qux");
+    eqint(strlen(value), 7);
+
+    eqint(0, headtok(NULL, &saveptr, &key, &value));
+    eqstr(key, "Connection");
+    eqint(strlen(key), 10);
+    eqstr(value, "corge");
+    eqint(strlen(value), 5);
+    
+    eqint(0, headtok(NULL, &saveptr, &key, &value));
+    eqstr(key, "Content-Length");
+    eqint(strlen(key), 14);
+    eqstr(value, "99");
+    eqint(strlen(value), 2);
+    
+    eqint(1, headtok(NULL, &saveptr, &key, &value));
+}
+
+
+void
 main() {
-    test_request_parser();
-    return EXIST_SUCCESS;
+    test_reqtok();
+    // test_reqtok_error();
+    test_headtok();
+    // test_headtok_error();
 }
